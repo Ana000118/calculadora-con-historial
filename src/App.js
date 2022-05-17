16feaf0a243657c57f8ad9ae62f863aff3b2c93f
ROJAS Juan Ana Karina <rojasjuananakarina@gmail.com>
2022-05-17 05:04:29 -0500

    inserción para los primeros números y operadores e inserción de estilos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+      <div className='row'>
+        <div className='col-8'>
+         <h2 align="center">Calculadora</h2>
+
+         <label>Primer Número</label>
+         <input 
+           />
+
+         <label>Segundo Número</label>
+         <input 
+           />
+   
+         <select >
+           <option>sumar</option>
+           <option>restar</option>
+           <option>multiplicar</option>
+           <option>Dividir</option>
+           
+         </select>
+           <label>Resultado </label>
+
+               </div>
+             </div>
+           </div>
+        
   );
 }
 
