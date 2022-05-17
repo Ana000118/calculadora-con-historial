45e08ea8e47a89b86cf845de29fb94d20e101bad
ROJAS Juan Ana Karina <rojasjuananakarina@gmail.com>
2022-05-17 05:42:35 -0500

    inserción de los botones guardar, borrar lista y borrar operación

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [inputState, setinputState] = useState({
+    numero1:"",
+    numero2:"",
+    Resultado:"",
+  });
+
   const [numero1, setnumero1]=useState(0);
   const [numero2, setnumero2]=useState(0);
   const [Resultado, setresultado]=useState(0);
   const [operacion, setoperacion]=useState('sumar');
 
+  const inicialState = JSON.parse(localStorage.getItem ("calculadora")) || [];
+  const [calculadora, setCalculadora] = useState(inicialState);     
+
+  const Guardar = () => {
+    setCalculadora([...calculadora, inputState]);
+    localStorage.setItem("calculadora", JSON.stringify(calculadora));
+    
+  };
+
+  const Borraroperacion = (index) => {
+    const nuevoArreglo = [];
+
+    calculadora.forEach((calculadora,i) => {
+      if (index !== i) {    
+        nuevoArreglo.push(calculadora);   
+      } 
+    }); 
+
+    localStorage.setItem("calculadora", JSON.stringify(nuevoArreglo)); 
+    setCalculadora([...nuevoArreglo]); 
+  };
+
+  const LimpiarLista = () => {
+    setCalculadora([])
+    localStorage.setItem("calculadora", JSON.stringify([]));
+
+  }
+  
   const calcular = () => {
     if (operacion === "sumar")
      return parseFloat(numero1) + parseFloat(numero2);
@@ -52,8 +86,51 @@ function App() {
            
          </select>
            <label>Resultado {Resultado}</label>
-           
+           <div className="col">
+          <span className="row ms-1">
+          
+          <button 
+            type="button"
+            className="btn btn-primary"
+            onClick={Guardar}
+            >
+            Guardar
+          </button>
+          </span>
+           </div>
+         </div>
+         <br></br>
+           <div className="col">
+             <div className="row ms-2">
+               <h2 align="center">Historial</h2>
+          {calculadora.length === 0 ? (
+            "Al momento no tienes notas guardadas. Puedes crear una en el formulario"
+            ) : (
+              <ol>
+                {calculadora.map((item, index) => {
+                  return(
+                    <li key ={index}>
+                      {item.numero1} ({item.numero2}) {item.Resultado}&nbsp;
 
+                      <i className="bi-x-circle-fill" 
+                      onClick={() => Borraroperacion (index)}
+                      style={{
+                      color:"#grey", 
+                      fontSize:"0.75rem", 
+                      cursor:"pointer",}}></i>
+                    </li>
+                  )
+                })}
+              </ol>
+            )}
+                 <button  
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={LimpiarLista}
+                    >
+                    borrar
+                 </button>
+               </div>
                </div>
              </div>
            </div>
